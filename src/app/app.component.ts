5943a81103046127559da9abd053a0d2a6077025
Ezekiel <ezekiel.de.leon@jci.com>
2023-03-24 15:03:10 -0700

    starter.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-course';
+  data = {
+    title: 'Angular Core Deep Dive!',
+  };
+
+  onLogoClick() {
+    console.log('hello world');
+  }
+
+  onKeyUp(newTitle: string) {
+    this.data.title = newTitle;
+  }
 }
